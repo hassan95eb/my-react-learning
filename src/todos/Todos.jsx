49d2602fb5ee025e19d10c6ecfd568c9e421fd26
hassan95eb <19h.amini95@gmail.com>
2024-06-11 13:42:37 +0330

    use reducer in simple button

--- src/todos/Todos.jsx
+++ src/todos/Todos.jsx
@@ -1,12 +1,15 @@
 import { useReducer } from "react";
 import style from "../style.module.css";
-const init = 0;
+const init = {
+  value1: 0,
+  value2: 0,
+};
 const reducer = (state, action) => {
   switch (action) {
     case "incremet":
-      return state + 1;
+      return { ...state, value1: state.value1 + 1 };
     case "decrement":
-      return state - 1;
+      return { ...state, value1: state.value1 - 1 };
     case "reset":
       return init;
     default:
@@ -25,7 +28,7 @@ const Todos = () => {
   return (
     <div className={`${style.item_content} mt-5 p-4 container-fluid`}>
       <h4 className="text-center">مدیریت کارها </h4>
-      <div>{count}</div>
+      <div>{count.value1}</div>
       <button onClick={() => dispatch("incremet")}>افزایش</button>
       <button onClick={() => dispatch("decrement")}>کاهش</button>
       <button onClick={() => dispatch("reset")}>ریست</button>
